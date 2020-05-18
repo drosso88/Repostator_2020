bbfa7ba3090ebbbceeb95ac5a2cedddafb07e496
rocio <rocio.soriano.gomez@gmail.com>
2020-05-18 12:10:38 +0200

    ya almacena repostages

--- app/src/main/java/com/example/repostator_2020/MainActivity.java
+++ app/src/main/java/com/example/repostator_2020/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.repostator_2020;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -25,5 +31,42 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        ArrayList<String> listadoRepostajes = new ArrayList<String>();
+        ListView listaVista; //vista uw muestra filas
+        //guarda cadenas de valores
+        SharedPreferences sp = getSharedPreferences("datos", Context.MODE_PRIVATE);
+//cargo la lista de repostajes de sharedPreferences al arraylist
+        int size = sp.getInt("listado_size", 0); //el tamaño de la lista de repostajes que hemos hecho
+        String precio = "";
+        String kilometros = "";
+        String litros = "";
+        String dia = "";
+        String mes = "";
+        String year = "";
+        double costeTotal = 0;
+        for(int i = 1 ; i <= size; i++){
+            precio = sp.getString("precio_" + i, "0");
+            kilometros = sp.getString("kilometros_" + i, "0");
+            litros = sp.getString("litros_" + i, "0");
+            costeTotal = (Double.valueOf(precio) * Double.valueOf(litros));
+            dia = String.valueOf( sp.getInt("dia_" + i, 0) );
+            mes = String.valueOf( sp.getInt("mes_" + i, 0) );
+            year = String.valueOf( sp.getInt("anyo_" + i, 0) );
+            listadoRepostajes.add(kilometros + " km " + litros + " L " + precio + " €/L coste: " + String.format("%.2f",
+                    costeTotal) + " fecha: " + dia + "-" + mes + "-" + year);
+        }
+//poblamos el listview con el arrayList
+        listaVista = (ListView) findViewById(R.id.marcoLista);
+//este es el array adapter, que conecta el arrayList con el activity
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                listadoRepostajes
+        );
+        listaVista.setAdapter(arrayAdapter);
     }
 }
